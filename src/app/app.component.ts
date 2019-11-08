407ef0e0828cd19e3a283981ed8b93edca30cd2b
Babu Suriyanarayanan <babu28537@gmail.com>
2019-11-08 10:28:34 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aws-academe';
+  signedIn: boolean;
+    user: any;
+    greeting: string;
+    constructor( private amplifyService: AmplifyService ) {
+        this.amplifyService.authStateChange$
+            .subscribe(authState => {
+                this.signedIn = authState.state === 'signedIn';
+                if (!authState.user) {
+                    this.user = null;
+                } else {
+                    this.user = authState.user;
+                    this.greeting = "Hello " + this.user.username;
+                    this.getCourses();
+                }
+        });
+    }
+    async getCourses() {
+        const data = await API.get('academecourses','/academecourses','');
+
+    }
 }
